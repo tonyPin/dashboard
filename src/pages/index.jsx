cf977215abbedf29ebf384804f221f037be54ab6
tonyPin <43199056+tonyPin@users.noreply.github.com>
2022-07-05 06:26:54 +0100

    files and setup completed

---
+++ src/pages/index.jsx
@@ -0,0 +1,16 @@
+export { default as Ecommerce } from './Ecommerce';
+export { default as Kanban } from './Kanban';
+export { default as Orders } from './Orders';
+export { default as Employees } from './Employees';
+export { default as Editor } from './Editor';
+export { default as Customers } from './Customers';
+export { default as ColorPicker } from './ColorPicker';
+export { default as Calendar } from './Calendar';
+export { default as Area } from './Area';
+export { default as Bar } from './Bar';
+export { default as ColorMapping } from './ColorMapping';
+export { default as Financial } from './Financial';
+export { default as Line } from './Line';
+export { default as Pie } from './Pie';
+export { default as Pyramid } from './Pyramid';
+export { default as Stacked } from './Stacked';
